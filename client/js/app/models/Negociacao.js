7b179b10b21cb3f5f821dd36abd6f1d9cc4d22f4
Rafael Souza <raffaelsouza908@gmail.com>
2019-12-09 12:25:17 -0300

    Add classe Negociacao

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,24 @@
+class Negociacao {
+    constructor(data, quantidade, valor){
+        this._data = new Date(data.getTime()); //programação defesiva para evitar alteração da data de um objeto.
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this) //Impede que as propriedades (qtd e valor) sejam alteradas
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+    get data(){
+        return new Date(this.getTime()); //programação defesiva para evitar alteração da data de um objeto.
+    }
+
+    get quantidade(){
+        return this._quantidade
+    }
+
+    get valor(){
+        return this._valor
+    }
+}
