b4150ecec1ba211841e485cf25595d0635745467
Rafael Souza <raffaelsouza908@gmail.com>
2019-12-11 13:40:19 -0300

    iniciando NegociacoesView

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -7,18 +7,22 @@ class Negociacao {
     }
 
     get volume(){
+
         return this._quantidade * this._valor;
     }
 
     get data(){
+
         return new Date(this._data.getTime()); //programação defesiva para evitar alteração da data de um objeto.
     }
 
     get quantidade(){
+
         return this._quantidade
     }
 
     get valor(){
+        
         return this._valor
     }
 }
