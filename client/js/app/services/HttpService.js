50b7cfba0f9e94fa08bddf5de20559e30e3165ce
Rafael Souza <raffaelsouza908@gmail.com>
2020-01-05 18:17:36 -0300

    HttpService refatorada, utilizando fetch API para fazer as requisições

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,28 +1,18 @@
 class HttpService {
 
+    _handleErros(res){
+
+        if(!res.ok)
+            throw new Error(res.statusText);
+        return res
+        
+    }
+
     get (url){
 
-        return new Promise( (resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-    
-            xhr.open('GET', url);
-    
-            xhr.onreadystatechange = () => {
-                
-                if(xhr.readyState == 4) {
-    
-                    if(xhr.status == 200){
-    
-                        resolve(JSON.parse(xhr.responseText))
-                    } else {
-                        reject(xhr. responseText);
-                    }
-                }
-            };
-    
-            xhr.send();
-        })
+        return fetch(url)
+            .then( res => this._handleErros(res) )
+            .then( res => res.json() )
     }
 
     post(url, dado) {
